2556ab6ac77854ee4fe3fa7fe30ef80f0a62ed42
joneconsulting <34033082+joneconsulting@users.noreply.github.com>
2020-08-06 00:07:57 +0900

    Update app.js

--- cloud-computing/app.js
+++ cloud-computing/app.js
@@ -3,7 +3,14 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+
+  var ip = req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  console.log(ip);
+  res.send('Hello, World! : ' + ip + "," + new Date());
+  
 });
 
 app.get('/health', (req, res) => {
