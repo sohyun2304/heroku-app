2086104bd827a0fc2d391f0343ffdc280251a0e0
joneconsulting <34033082+joneconsulting@users.noreply.github.com>
2020-08-05 23:55:58 +0900

    Update app.js

--- cloud-computing/app.js
+++ cloud-computing/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send();
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
