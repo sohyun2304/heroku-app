6fd35e9e90d605d30b0be3dc6ab37caf2e132bed
joneconsulting <34033082+joneconsulting@users.noreply.github.com>
2020-08-06 14:26:27 +0900

    Update index3.js

--- aws/nodejs_app/index3.js
+++ aws/nodejs_app/index3.js
@@ -9,7 +9,12 @@ const app = express();
 app.set('port', process.env.PORT || 8000);
 
 app.get('/', (req, res) => {
-  res.send('Root');
+    var ip = req.headers['x-forwarded-for'] || 
+                req.connection.remoteAddress || 
+                req.socket.remoteAddress ||
+            (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    console.log(ip);
+    res.send('Hello, World! : ' + ip + "," + new Date());
 });
 
 app.get('/users', (req, res) => {
